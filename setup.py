e10e490e2649f9db1181e368bd3d4bb2e9ddec79
mattdodge <mattedgod@gmail.com>
2019-06-24 20:55:24 -0700

    Add MLB yahoo stat mappings

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+reqs = [
+    'requests',
+    'xmljson',
+]
+
 setuptools.setup(
     name="yahoofantasy",
-    version="0.0.1",
+    version="0.0.2",
     author="Matt Dodge",
     author_email="matt@dodge.com",
     description="An SDK for the Yahoo! Fantasy Sports API",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=reqs,
     url="https://github.com/mattdodge/yahoofantasy",
     packages=setuptools.find_packages(),
     classifiers=[

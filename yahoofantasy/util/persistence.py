b4c64ae0ea18cd2e84800c68f93b765dea2a87f6
Matt Dodge <mattedgod@gmail.com>
2020-03-18 22:28:06 -0700

    Use an in-memory cache for persistence too

--- yahoofantasy/util/persistence.py
+++ yahoofantasy/util/persistence.py
@@ -5,7 +5,11 @@ import pickle
 from time import time
 
 _DEFAULT_SINGLETON = "__secret_default_function_argument"
-DEFAULT_TTL = 1800
+DEFAULT_TTL = 3600  # 1.5 hours of persistence by default
+# An in-memory cache of the current persistence
+# This won't work if multiple threads are writing to the persitence file,
+# but I think we're ok for now
+CURRENT_PERSISTENCE = {}
 
 
 def get_persistence_filename(persist_key):
@@ -21,6 +25,7 @@ def save(save_path, save_val, persist_key="", overwrite=False):
         persist_key (str): A unique identifier for the persistence file
         overwrite (bool): If True, will wipe out existing persistence
     """
+    global CURRENT_PERSISTENCE
     filename = get_persistence_filename(persist_key)
     persisted_data = {}
     if not overwrite and isfile(filename):
@@ -31,6 +36,8 @@ def save(save_path, save_val, persist_key="", overwrite=False):
     persisted_data = set_(persisted_data, save_path, save_val)
     with open(filename, 'wb') as fp:
         pickle.dump(persisted_data, fp)
+    CURRENT_PERSISTENCE = set_(CURRENT_PERSISTENCE, save_path + '__time', time())
+    CURRENT_PERSISTENCE = set_(CURRENT_PERSISTENCE, save_path, save_val)
 
 
 def load(load_path, default=_DEFAULT_SINGLETON, ttl=DEFAULT_TTL, persist_key=""):
@@ -40,12 +47,17 @@ def load(load_path, default=_DEFAULT_SINGLETON, ttl=DEFAULT_TTL, persist_key="")
         load_path (str): A pydash path to where the data is stored in persistence
         persist_key (str): A unique identifier for the persistence file
     """
-    filename = get_persistence_filename(persist_key)
-    if not isfile(filename):
-        persisted_data = {}
+    global CURRENT_PERSISTENCE
+    if CURRENT_PERSISTENCE:
+        persisted_data = CURRENT_PERSISTENCE
     else:
-        with open(filename, 'rb') as fp:
-            persisted_data = pickle.load(fp)
+        filename = get_persistence_filename(persist_key)
+        if not isfile(filename):
+            persisted_data = {}
+        else:
+            with open(filename, 'rb') as fp:
+                persisted_data = pickle.load(fp)
+                CURRENT_PERSISTENCE = persisted_data
     time_saved = get(persisted_data, load_path + '__time', 0)
     if ttl >= 0 and time_saved + ttl < time():
         logger.info("Persistence data expired, ignoring")
